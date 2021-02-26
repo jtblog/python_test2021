5da8b77fb67c6355266404a93465fc83a867bb69
Joseph T. Obagbemisoye <jtob91@yahoo.com>
2021-02-26 04:21:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, request, response, jsonify
+from flask import Flask, request, Response, jsonify
 from flask_mongoengine import MongoEngine
 import datetime
+import json
 #from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config['MONGODB_SETTINGS'] = {
-    'db' : 'db_name',
+    'db' : 'audio',
     'host' : 'localhost',
-    'db' : '27017'
+    'port' : 27017
 }
 #app.config['MONGODB_SETTINGS'] = {
 #    'host' : 'mongodb://localhost/audio'
@@ -19,106 +20,98 @@ db.init_app(app)
 class cDateTimeField(db.DateTimeField):
     def validate(self, value):
         super(cDateTimeField, self).validate(value)
-        #if(value < datetime.datetime.utcnow):
-        #    self.error('DateTime cannot be in the past')
+        if(value < datetime.datetime.utcnow()):
+            self.error('DateTime cannot be in the past')
 
-class Song(db.EmbeddedDocument):
+class audioMetaData(db.DynamicEmbeddedDocument):
     id = db.IntField(required = True, unique = True)
     name = db.StringField(required = True, max_length = 100)
     duration = db.IntField(required = True, min_value = 1)
-    #upload_time = cDateTimeField(required = True, default = datetime.datetime.utcnow)
+    upload_time = cDateTimeField(required = True, default = datetime.datetime.utcnow())
+    participant = db.ListField(db.StringField(max_length = 100))#, default = [])
+        
+class Audio(db.DynamicDocument):
+    audioFileType = db.StringField(required = True, max_length = 100)
+    audioFileMetadata = db.DynamicField(audioMetaData)
 
-class Podcast(db.EmbeddedDocument):
-    id = db.IntField(required = True, unique = True)
-    name = db.StringField(required = True, max_length = 100)
-    duration = db.IntField(required = True, min_value = 1)
-    #upload_time = cDateTimeField(required = True, default = datetime.datetime.utcnow)
-    host = db.StringField(required = True, max_length = 100)
-    participant = db.ListField(db.StringField(max_length = 100), default = [])
-
-class Audiobook(db.EmbeddedDocument):
-    id = db.IntField(required = True, unique = True)
-    name = db.StringField(required = True, max_length = 100)
-    duration = db.IntField(required = True, min_value = 1)
-    #upload_time = cDateTimeField(required = True, default = datetime.datetime.utcnow)
-    author = db.StringField(required = True, max_length = 100)
-    narrator = db.StringField(required = True, max_length = 100)
-
-#class metaData(db.DynamicEmbeddedDocument):
-#    pass
-
-class Audio(db.Document):
-    fileType = db.StringField(required = True, max_length = 100)
-    #metadata = db.EmbeddedDocumentField(???)
-
-def sortedAudio(mdata):
-    if('host' in mdata or 'participant' in mdata):
-        return Podcast(mdata)
-    elif('author' in mdata or 'narrator' in mdata):
-        return Audiobook(mdata)
-    else:
-        return Song(mdata)
-
+'''
 @app.route('/')
 def index():
-    return '<h1>Hello!</h1>' + datetime.datetime.utcnow
+    return '<h1>Hello! It is {}</h1>'.format(datetime.datetime.utcnow())
+'''
 
-@app.route('/api/create', method =['POST'])
+def _ctask(args1, args2):
+    return args2
+
+def _metadatachk(value):
+    if('host' in value):
+        if not bool(value['host'].strip()):
+            return Response("Host cannot be empty", status = 400)
+        if(len(value['host']) > 100):
+            return Response("Host cannot be larger than 100 characters", status = 400)
+    pass
+    if('author' in value or 'narrator' in value):
+        if not bool(value['author'].strip()):
+            return Response("Author cannot be empty", status = 400)
+        if(len(value['author']) > 100):
+            return Response("Author cannot be larger than 100 characters", status = 400)
+        if not bool(value['narrator'].strip()):
+            return Response("Narrator cannot be empty", status = 400)
+        if(len(value['narrator']) > 100):
+            return Response("Narrator cannot be larger than 100 characters", status = 400)
+    pass
+
+
+@app.route('/api/create', methods = ['POST'])
 def create():
-    body = request.json
+    body = request.get_json(force=True)
     ft = None
     fmd = None
     if('audioFileType' not in body or 'audioFileMetadata' not in body):
-        return response("Bad Request", status = 400)
+        return Response("Bad Request", status = 400)
     else:
         ft = body['audioFileType']
         fmd = body['audioFileMetadata']
-        dt = sortedAudio(fmd)
-        audio = Audio( audioFileType = ft, audioFileMetadata = dt).save()
-        return jsonify(audio), 200
 
-@app.route('/api/delete/<audioFileType>/<audioFileID>', method =['DELETE'])
+        _audiometadata = audioMetaData(**fmd)
+        chk = None
+        chk = _metadatachk(fmd)
+        
+        audio = Audio( audioFileType = ft)
+        audio.audioFileMetadata = _audiometadata
+
+        return chk if chk!=None else _ctask(audio.save(), jsonify(audio)), 200
+
+@app.route('/api/delete/<audioFileType>/<audioFileID>', methods =['DELETE'])
 def delete(audioFileType, audioFileID):
-    audio = Audio.objects.get_or_404(audioFileMetadata__id = audioFileID)
+    audio = Audio.objects.filter(audioFileMetadata__id = audioFileID).first()
     audio.delete()
     return jsonify(str(audioFileID)), 200
 
-@app.route('/api/update/<audioFileType>/<audioFileID>', method =['PUT'])
+@app.route('/api/update/<audioFileType>/<audioFileID>', methods =['PUT'])
 def update(audioFileType, audioFileID):
-    body = request.json
+    body = request.get_json(force=True)
     ft = None
     fmd = None
 
     if('audioFileType' not in body or 'audioFileMetadata' not in body):
-        return response("Bad Request", status = 400)
+        return Response("Bad Request", status = 400)
     else:
         fmd = body['audioFileMetadata']
 
-        audio = Audio.objects.get_or_404(audioFileMetadata__id = audioFileID)
+        audio = Audio.objects.filter(audioFileMetadata__id = audioFileID).first()
         ft = audio.audioFileType
-        dt = sortedAudio(fmd)
-        audio.update(audioFileType = ft, audioFileMetadata = dt)
-        return jsonify(audio), 200
+        _audiometadata = audioMetaData(**fmd)
+        chk = None
+        chk = _metadatachk(fmd)
+        return chk if chk!=None else _ctask(audio.update(audioFileType = ft, audioFileMetadata = _audiometadata), jsonify(audio)), 200
     
-@app.route('/api/get/<audioFileType>', method =['GET'])
+@app.route('/api/get/<audioFileType>', methods =['GET'])
 def get(audioFileType):
-    audio = Audio.objects.get_or_404(audioFileType = audioFileType)
-    return audio.to_dict(), 200
-
-@app.route('/api/get/<audioFileType>/<audioFileID>', method =['GET'])
-def _get(audioFileType, audioFileID):
-    audio = Audio.objects.get_or_404(audioFileMetadata__id = audioFileID)
+    audio = Audio.objects.filter(audioFileType = audioFileType)
     return jsonify(audio), 200
 
-#200 Ok
-#400 bad request
-#500 internal server error
-
-#app.config["MONGO_URI"]= ""
-#mongo = PyMongo(app)
-#client = pymongo.MongoClient("mongodb+srv://joetfx:<password>@cluster0.nxrlt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-#db = client.test
-
-#@app.route('/<name>')
-#def index(name):
-#    return '<h1>Hello {} !</h1>'.format(name)
+@app.route('/api/get/<audioFileType>/<audioFileID>', methods =['GET'])
+def _get(audioFileType, audioFileID):
+    audio = Audio.objects.filter(audioFileMetadata__id = audioFileID).first()
+    return jsonify(audio), 200
